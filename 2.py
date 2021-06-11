3fb0e61dff52df29f3e3c36e8865a7cc863eb22d
kr-utsav <8579931993@gitam.in>
2021-06-11 13:24:25 +0530

    Perfect or not

---
+++ 2.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number"))
+add=0
+for i in range(1,a):
+ if a%i==0:
+    print(i)
+    add+=i
+if add==a:
+ print("Prefect no.")
+else:
+ print("Not Perfect no.")
+print("Utsav",222010308053)
